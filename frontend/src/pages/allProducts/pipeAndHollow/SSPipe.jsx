776cf33b0d6a00c516f7ae50274511f6690e6896
Subhradip <subhradipdas6969@gmail.com>
2024-10-05 10:48:34 +0530

    ss

--- frontend/src/pages/allProducts/pipeAndHollow/SSPipe.jsx
+++ frontend/src/pages/allProducts/pipeAndHollow/SSPipe.jsx
@@ -1,65 +1,259 @@
+/* eslint-disable react/no-unescaped-entities */
 import Navbar from "../../../include/Navbar";
 import logo from "../../../assets/Logo.jpg";
+
 function SSPipe() {
-    return (
-        <>
-            <Navbar />
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row mt-3">
+          <div className="col-md-2">
+            <img
+              src={logo}
+              alt=""
+              style={{ width: "100px", height: "100px" }}
+            />
+          </div>
+          <div className="col-md-10">
             <div className="container">
-                <div className="row mt-3">
-                    <div className="col-md-2">
-                        <img src={logo} alt="" style={{ width: '100px', height: '100px' }} />
-                    </div>
-                    <div className="col-md-10">
-                        <div className="container">
-                            <h3>lorem ipsum</h3>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus, ea animi earum illo pariatur, voluptatibus magni commodi, nam nemo id itaque cum in sed error provident officia incidunt debitis!
-                        </div>
-                    </div>
-                </div>
+              <h3>lorem ipsum</h3>
+              We SellHigh Quality Stainless Steel Pipes and Tubes at reasonable
+              prices! <br />
+              Our Key Products in Stainless Steel - Pipes, Butt Welding
+              Fittings, Forged Fittings, 180 Degree Elbows, 90 Degree Elbows, 60
+              Degree Elbows, 45 Degree Elbows, 30 Degree Elbows, Equal Tee,
+              Reducing Tee, Concentric Reducer, Eccentric Reducer, Lap Joint
+              Stub End, Olet, Cap, Bend, Cross, Coupling, Stainless Steel
+              Lateral Tee, Bellows Expansion Joints, Flexible Metal Hose,
+              Standard/Custom Fittings, Bleed & Flushing Rings
+            </div>
+          </div>
+        </div>
 
-                <div className="row mt-5">
-                    <div className="col-md-6">
-                        <h3>lorem ipsum</h3>
-                        <table className="table table-hover table-striped-columns">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colSpan={2}>Larry the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                    </div>
-                    <div className="col-md-6">
-                        <div className="container">
-                            <h3>lorem ipsum</h3>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A rerum officiis deleniti perferendis doloribus labore sit tenetur molestias aspernatur, quaerat nemo iste placeat obcaecati at. Sapiente repellat nesciunt aut aliquam.
-                        </div>
-                    </div>
+        <div className="row mt-5 mb-5">
+          <div className="col-md-6">
+            <h3>Product Specifications</h3>
+            <div className="card shadow-lg rounded">
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table className="table table-bordered table-striped table-hover">
+                    <thead className="thead-dark">
+                      <tr>
+                        <th scope="col">Product</th>
+                        <th scope="col">Stainless Steel Pipes & Tubes</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">Standards</th>
+                        <td>
+                          ASTM A312, ASTM A213, ASTM A269, ASTM A270, ASTM A554,
+                          ASTM A249
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Grades</th>
+                        <td>
+                          TP304, TP304L, TP304/304L, TP310S, TP316, TP316L,
+                          TP316/316L, TP317L, TP321, TP347H, S31803, 904L
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Type</th>
+                        <td>Seamless, Welded, Fabricated</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">NB Size</th>
+                        <td>
+                          1/2" to 48", Thickness: SCH 5S, 10S, 40S (STD), 80S
+                          (XS), 160, XXS
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">OD Size</th>
+                        <td>O.D.: Φ 6 to $ 406mm, W.T: 1 mm to 30 mm</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Specification</th>
+                        <td>ASTM, ASME, DIN</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Finish</th>
+                        <td>Annealed & Pickled, 2B, No.4, Mirror, Polish</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Packing</th>
+                        <td>Bundled Packing</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Services</th>
+                        <td>Cutting, Beveling, Threading, Polishing</td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </div>
-            </div>        </>
-    );
+              </div>
+            </div>
+
+            <div className="mt-5">
+              <img
+                src="https://matrixalloys.com/wp-content/uploads/2019/11/welded-pipe-tube.jpg"
+                alt="Stainless steel welded pipes"
+                className="img-fluid shadow-lg rounded"
+                style={{ width: "500px" }}
+              />
+            </div>
+            <div className="mt-5 mb-3">
+              <img
+                src="https://im01.itaiwantrade.com/aac5539e-68c9-4dfd-a3a0-05966155db1d/304_stainless_tubes_and_pipes.jpg"
+                alt="Stainless steel tubes and pipes"
+                className="img-fluid shadow-lg rounded"
+                style={{ width: "500px" }}
+              />
+            </div>
+          </div>
+
+          <div className="col-md-6">
+            <div className="container">
+              <h3>lorem ipsum</h3>
+              Stainless Steel Seamless Tubing, Stainless Steel Instrumentation
+              Tubes, High Pressure Stainless Steel Tubes, Seamless Cold Rolled
+              Coiled Stainless Steel Tubing, Stainless Steel Capillary Tubing
+              Pre Insulated Stainless Steel Tubes, Electropolished Stainless
+              Steel Tubes, Large Diameter Fabricated Pipes, Large Size Seamless
+              Pipes. Seamless Heat Exchanger Tubes, Seamless U-tubes, Seamless
+              U- Bended Heat Exchanger Tubes, Decorative Polished Pipes, Round
+              Pipes, Square Pipes, Rectangular Pipes
+            </div>
+            <div className="container my-5">
+              <h2 className="text-center mb-4">
+                Austenitic Stainless Steel Capillary Tubes Supplier
+              </h2>
+              <p className="text-muted text-center">
+                Cold Drawn and Annealed Capillary Tubes
+              </p>
+
+              <div className="mb-4">
+                <p>
+                  <strong>METLINE</strong> is a leading stockholder and supplier
+                  of stainless steel capillary tubing for use in many high
+                  precision systems, available in a wide range of stainless
+                  steels, mainly 304 and 316 grades. Our capillary tube products
+                  are characterised by tight tolerance, precise precut lengths,
+                  clean inner surface, and smooth surface finish.
+                </p>
+              </div>
+              <div className="card shadow-lg rounded">
+                <div className="card-body">
+                  <div className="table-responsive">
+                    <table className="table table-bordered table-striped table-hover mt-3">
+                      <tbody>
+                        <tr>
+                          <td colSpan={2} className="text-center">
+                            <h4>Specifications</h4>
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Outer Diameter</th>
+                          <td>0.3mm - 10mm</td>
+                        </tr>
+                        <tr>
+                          <th>Wall Thickness</th>
+                          <td>0.08mm - 1.5 mm</td>
+                        </tr>
+                        <tr>
+                          <th>Length</th>
+                          <td>Custom Lengths</td>
+                        </tr>
+                        <tr>
+                          <th>Material Grade</th>
+                          <td>SS 304, SS316</td>
+                        </tr>
+                        <tr>
+                          <th>Size Range</th>
+                          <td>1/8" to 48", Seamless Pipes up to 24"</td>
+                        </tr>
+                        <tr>
+                          <th>Thickness</th>
+                          <td>
+                            0.4mm up to 30.00mm, Schedules 5, 10, 40, 80S, XS,
+                            160, XXH
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Length</th>
+                          <td>Standard 6-6.1 meter, 20 Ft, Custom Lengths</td>
+                        </tr>
+                        <tr>
+                          <th>Standards</th>
+                          <td>
+                            ASTM A312, ASME SA312, ASTM A269, ASTM A213, ASTM
+                            A511, ASTM A249, ASTM A270, ASTM A554
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Product Shapes</th>
+                          <td>Round, Square, Rectangular</td>
+                        </tr>
+                        <tr>
+                          <th>Surface Finish</th>
+                          <td>
+                            Solution Annealed, Pickled, Matt No.4, BA Polishing,
+                            No.8 (8K)
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Types</th>
+                          <td>
+                            Seamless, Welded, Extruded, Structural, Hot
+                            Finished, Cold Drawn, Annealed & Pickled
+                          </td>
+                        </tr>
+                        <tr>
+                          <td colSpan={2} className="text-center">
+                            <h4>Additional Information</h4>
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Mill Test Certificates</th>
+                          <td>
+                            As per EN 10204 3.1, NABL Approved Labs, 3.2
+                            Certification by Third Party
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Product Range</th>
+                          <td>
+                            High Pressure Tubes, Seamless Cold Rolled Coiled
+                            Tubes, Decorative Polished Pipes
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>American Standard Grades</th>
+                          <td>
+                            304, 304L, 316, 316L, 321, 316Ti, 304H, 347H,
+                            Duplex, Superduplex
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>European Standard Grades</th>
+                          <td>
+                            1.4301, 1.4401, 1.4571, 1.4841, 1.4435, 1.4006,
+                            1.4462
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SSPipe;
