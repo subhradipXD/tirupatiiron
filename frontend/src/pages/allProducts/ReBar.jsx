ae05dc510cc13397e1220ff0b36a9e1a4d685e55
Subhradip <subhradipdas6969@gmail.com>
2024-10-15 20:38:01 +0530

    fix

--- frontend/src/pages/allProducts/ReBar.jsx
+++ frontend/src/pages/allProducts/ReBar.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../../include/Navbar";
-import REBAR from "../../assets/ReBar.jpeg";
+import REBAR from "../../assets/ReBar.png";
 
 function ReBar() {
     return (
@@ -12,22 +12,25 @@ function ReBar() {
                     </div>
                     <div className="col-12 col-md-8">
                         <div className="container">
-                            <h3>Lorem Ipsum</h3>
+                            <h3>Re-Bar</h3>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-                                ducimus, ea animi earum illo pariatur, voluptatibus magni commodi,
-                                nam nemo id itaque cum in sed error provident officia incidunt debitis!
+                                When does a house become a home? When the members of the household live in a
+                                safe and secure environment.
+                                Building a structure that is inherently strong and is able to resist external forces such as
+                                earthquakes and other natural forces lays the foundation of a safe home.
+                                <br />
+                                Thermo Mechanically Treated (TMT) bar is a type of reinforced steel bar which undergoes
+                                an additional process during its production cycle to obtain these required characteristics.
+                                This additional processes are Quenching and Self-Tempering.
+
                             </p>
                         </div>
                     </div>
                 </div>
 
-
-
-
                 <div className="row mt-5">
                     <div className="col-md-6">
-                        <h3>lorem ipsum</h3>
+                        <h3>Specification</h3>
                         <table className="table table-hover table-striped-columns">
                             <thead>
                                 <tr>
@@ -96,70 +99,114 @@ function ReBar() {
                         </table>
                     </div>
 
-
                     <div className="col-md-6">
                         <div className="container">
                             <h3>lorem ipsum</h3>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A rerum officiis deleniti perferendis doloribus labore sit tenetur molestias aspernatur, quaerat nemo iste placeat obcaecati at. Sapiente repellat nesciunt aut aliquam.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. A rerum
+                            officiis deleniti perferendis doloribus labore sit tenetur
+                            molestias aspernatur, quaerat nemo iste placeat obcaecati at.
+                            Sapiente repellat nesciunt aut aliquam.
                         </div>
 
                         <div className="container mt-3">
-                            <div className="card text-center mb-3 card-hover" data-bs-toggle="modal" data-bs-target="#cardModal1" >
+                            <div
+                                className="card text-center mb-3 card-hover"
+                                data-bs-toggle="modal"
+                                data-bs-target="#cardModal1"
+                            >
                                 <div className="card-body">
                                     <h5 className="card-title">NPB + WPB</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cardModal1">Go somewhere</a>
+                                    <p className="card-text">
+                                        With supporting text below as a natural lead-in to
+                                        additional content.
+                                    </p>
+                                    <a
+                                        href="#"
+                                        className="btn btn-primary"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#cardModal1"
+                                    >
+                                        Go somewhere
+                                    </a>
                                 </div>
                             </div>
 
-                            <div className="card text-center mb-3 card-hover" data-bs-toggle="modal" data-bs-target="#cardModal2" >
+                            <div
+                                className="card text-center mb-3 card-hover"
+                                data-bs-toggle="modal"
+                                data-bs-target="#cardModal2"
+                            >
                                 <div className="card-body">
                                     <h5 className="card-title">Universal Column (UC)</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a href="#" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cardModal2">Go somewhere</a>
+                                    <p className="card-text">
+                                        With supporting text below as a natural lead-in to
+                                        additional content.
+                                    </p>
+                                    <a
+                                        href="#"
+                                        className="btn btn-primary"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#cardModal2"
+                                    >
+                                        Go somewhere
+                                    </a>
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
                 </div>
             </div>
-
-
-
-
 
             {/* modal */}
-            <div className="modal fade" id="cardModal1" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div
+                className="modal fade"
+                id="cardModal1"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
                 <div className="modal-dialog modal-lg">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Card 1 Modal</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5 className="modal-title" id="exampleModalLabel">
+                                Card 1 Modal
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
                         </div>
-                        <div className="modal-body">
-                            Content for card 1 modal...
-                        </div>
+                        <div className="modal-body">Content for card 1 modal...</div>
                     </div>
                 </div>
             </div>
 
-            <div className="modal fade" id="cardModal2" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div
+                className="modal fade"
+                id="cardModal2"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
                 <div className="modal-dialog modal-lg">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Card 2 Modal</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5 className="modal-title" id="exampleModalLabel">
+                                Card 2 Modal
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
                         </div>
-                        <div className="modal-body">
-                            Content for card 2 modal...
-                        </div>
+                        <div className="modal-body">Content for card 2 modal...</div>
                     </div>
                 </div>
             </div>
-
-
         </>
     );
 }
